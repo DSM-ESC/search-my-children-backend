82e47391872a6c4da63cef46c8183b7ebc5acf9a
민기남친지농이 <jinhong0719@naver.com>
2019-12-02 19:58:37 +0900

    [MODIFY] <User/services.py> 메서드 반환 값 type hint 추가

--- repo/User/services.py
+++ repo/User/services.py
@@ -11,5 +11,5 @@ class UserService(object):
         User(user_id=user_data[0], user_pw=user_data[1], name=user_data[2], birth_date=user_data[3], sex=user_data[4]).save()
 
     @staticmethod
-    def check_id_pw_match(user_id: str, user_pw: str):
+    def check_id_pw_match(user_id: str, user_pw: str) -> bool:
         return True if User.objects.get(user_id=user_id).user_pw == user_pw else False
