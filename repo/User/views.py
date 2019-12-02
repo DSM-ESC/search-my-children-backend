d55a87303177acb31d3e599d8ffc9df9a5f09d5f
민기남친지농이 <jinhong0719@naver.com>
2019-12-02 19:40:48 +0900

    [MODIFY] <User/views.py> signup API 전체적인 로직 구현

--- repo/User/views.py
+++ repo/User/views.py
@@ -2,7 +2,27 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from .serializers import (
+    SignUpSerializers
+)
+from .services import (
+    UserService
+)
+from .exceptions import (
+    UserIdIsExist
+)
+
 
 class SignUpAPI(APIView):
     def post(self, request):
-        return Response("", status=status.HTTP_200_OK)
+        serializer = SignUpSerializers(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        data = serializer.initial_data
+        if UserService.check_id_exists(user_id=data["user_id"]):
+            raise UserIdIsExist
+
+        UserService.create_new_user(data["user_id"], data["user_pw"], data["name"], data["birth_date"], data["sex"])
+        return Response(data, status=status.HTTP_200_OK)
