708960d4a5ca3f455cd6e9f53752e901571fce4e
민기남친지농이 <jinhong0719@naver.com>
2019-12-02 19:56:32 +0900

    [ADD] <User/exceptions.py> 아이디 비번 불일치 exception 추가

--- repo/User/exceptions.py
+++ repo/User/exceptions.py
@@ -2,5 +2,10 @@ from rest_framework.exceptions import APIException
 
 
 class UserIdIsExist(APIException):
-    status_code = 470
+    status_code = 471
     default_detail = "This id is already exists."
+
+
+class PasswordIsNotMatch(APIException):
+    status_code = 470
+    default_detail = "Id and password are not match."
