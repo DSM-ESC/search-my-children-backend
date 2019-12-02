ccc9ce6e683e1f3eee7a6473e4fe89b70627c9f2
민기남친지농이 <jinhong0719@naver.com>
2019-12-02 22:52:19 +0900

    [ADD] <Children/views.py> 'children/' 전체 로직 구현

--- repo/Children/views.py
+++ repo/Children/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+import jwt
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+
+from User.services import JWTService
+from .exceptions import (
+    NoIncludeJwt,
+    InappropriateJwt,
+    ExpiredJwt
+)
+from .services import (
+    ChildrenService
+)
+
+
+class ChildrenHandler(APIView):
+    def post(self, request):
+        try:
+            _id = JWTService.decode_jwt_to_pk(request.headers['Authorization'])
+        except KeyError:
+            raise NoIncludeJwt
+        except jwt.exceptions.DecodeError:
+            raise InappropriateJwt
+        except jwt.exceptions.ExpiredSignatureError:
+            raise ExpiredJwt
+
+        data = request.data
+        item_list = list(data.items())
+
+        if not 0 < len(item_list) <= 5 or not data:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        for item in item_list:
+            ChildrenService.create_new_children(_id, item[0], item[1])
+
+        return Response(_id, status=status.HTTP_200_OK)
