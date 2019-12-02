a7ce45cd3cf4e5e45ffc0678de6ae32926142628
민기남친지농이 <jinhong0719@naver.com>
2019-12-02 19:57:50 +0900

    [ADD] <User/service.py> 아이디 비밀번호 일치 확인 메서드 추가

--- repo/User/services.py
+++ repo/User/services.py
@@ -9,3 +9,7 @@ class UserService(object):
     @staticmethod
     def create_new_user(*user_data) -> None:
         User(user_id=user_data[0], user_pw=user_data[1], name=user_data[2], birth_date=user_data[3], sex=user_data[4]).save()
+
+    @staticmethod
+    def check_id_pw_match(user_id: str, user_pw: str):
+        return True if User.objects.get(user_id=user_id).user_pw == user_pw else False
