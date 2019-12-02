6a1d3b7bfa9d37ba2f717528f5b2d7251d9b30a4
민기남친지농이 <jinhong0719@naver.com>
2019-12-02 19:40:02 +0900

    [ADD] <User/services.py> 새 User 생성 메서드 추가

--- repo/User/services.py
+++ repo/User/services.py
@@ -5,3 +5,7 @@ class UserService(object):
     @staticmethod
     def check_id_exists(user_id: str) -> bool:
         return True if len(User.objects.filter(user_id=user_id).values()) else False
+
+    @staticmethod
+    def create_new_user(*user_data) -> None:
+        User(user_id=user_data[0], user_pw=user_data[1], name=user_data[2], birth_date=user_data[3], sex=user_data[4]).save()
