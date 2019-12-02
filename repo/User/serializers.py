5833748f5a091e473b53f8da2f18ab9431dc51d1
민기남친지농이 <jinhong0719@naver.com>
2019-12-02 18:32:35 +0900

    [CREATE] <User/serializers.py> signupAPI용 serializer 생성ㅎ

---
+++ repo/User/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+
+class SignUpSerializers(serializers.Serializer):
+    user_id = serializers.CharField(max_length=100)
+    user_pw = serializers.CharField()
+    name = serializers.CharField(max_length=10)
+    birth_date = serializers.CharField(max_length=10)
+    sex = serializers.CharField(max_length=10)
