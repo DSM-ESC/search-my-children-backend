0e479168b218601d89d0b8941f336a90f16c1f8c
민기남친지농이 <jinhong0719@naver.com>
2019-12-02 18:28:45 +0900

    [ADD] <User/models.py> User 모델 테이블 생성

--- repo/User/models.py
+++ repo/User/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user_id = models.CharField(max_length=100)
+    user_pw = models.TextField()
+    name = models.CharField(max_length=10)
+    birth_date = models.CharField(max_length=10)
+    sex = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.user_id
+
