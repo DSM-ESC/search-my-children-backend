a01c6bbba6ab76ded6db80d7ee1d668bbc86f57b
민기남친지농이 <jinhong0719@naver.com>
2019-12-02 19:57:13 +0900

    [ADD] <User/serializers.py> Login API용 serializer 생성

--- repo/User/serializers.py
+++ repo/User/serializers.py
@@ -7,3 +7,8 @@ class SignUpSerializers(serializers.Serializer):
     name = serializers.CharField(max_length=10)
     birth_date = serializers.CharField(max_length=10)
     sex = serializers.CharField(max_length=10)
+
+
+class LoginSerializers(serializers.Serializer):
+    user_id = serializers.CharField(max_length=100)
+    user_pw = serializers.CharField(max_length=100)
